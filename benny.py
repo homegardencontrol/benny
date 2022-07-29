8cb7f7e428c3321a1b78a7b966475b0f272a6514
Home Garden Control <109976409+homegardencontrol@users.noreply.github.com>
2022-07-29 20:07:05 +0700

    Update benny.py

--- benny.py
+++ benny.py
@@ -1,8 +1,9 @@
-print('Benny 0.91')
-print('==========')
-print('base on Python 3')
-print('Type "help", "copyright", "credits" or "license" for more information.')
-# define clear function
+def __benny__head__():
+	print('Benny 0.91')
+	print('==========')
+	print('base on Python 3')
+	print('Type "cls", "help", "copyright", "credits" or "license" for more information.')
+	# define clear function
 def cls():
 	import os
 	from subprocess import call
@@ -11,6 +12,8 @@ def cls():
 		_ = call('clear')
 	else:
 		_ = os.system('cls')
+	print(__benny__head__())
+cls()
 __benny__value__ = ''
 __benny__list__ = []
 def benny(name):
@@ -22,6 +25,8 @@ def bennylist(name):
 while True:
 	print('>>>',end=' ')
 	__benny__syntax__ = input()
+	if __benny__syntax__=='cls':
+		cls()
 	if __benny__syntax__=='help':
 		print('Type help() for interactive help, or help(object) for help about object.')
 	if __benny__syntax__=='copyright':
@@ -67,6 +72,16 @@ while True:
 			exec(__benny__syntax__)
 		except Exception as e: print(e)
 		continue
+	if __benny__syntax__.find('.')<0 and len(__benny__syntax__.split('['))==2 and __benny__syntax__.find('=')>__benny__syntax__.find('[') and __benny__syntax__.find('=')<__benny__syntax__.find(']'):
+		__benny__object__ = __benny__syntax__
+		__benny__list__ = __benny__object__[__benny__object__.find('[')+1:__benny__object__.find(']')].split(',')
+		__benny__object__=__benny__object__[0:__benny__object__.find('[')]
+		if not __benny__object__ in locals():
+			exec('class '+__benny__object__+':\n __benny__list__=[]\n pass')
+		for __benny__inlist__ in __benny__list__:
+			exec(__benny__object__+'.__benny__list__.append("'+__benny__inlist__[0:__benny__inlist__.find('=')].strip()+'")')
+			exec(__benny__object__+'.'+__benny__inlist__)
+		continue
 	if __benny__syntax__.find('.')<__benny__syntax__.find('=') and __benny__syntax__.find('.')>=0:
 		if len(__benny__syntax__.split('['))>2 or len(__benny__syntax__.split(']'))>2 or __benny__syntax__.find('{')>=0:
 			print('Benny error more than [1 block] in sentense and dict not allow')
@@ -103,11 +118,11 @@ while True:
 						exec(__root__dot__object__+'.'+__benny__object__+'='+__root__object__+'_'+__benny__object__)
 			if __benny__list__type__:
 				for __benny__inlist__ in __benny__list__:
-					exec(__root__object__+'.'+__benny__object__+'.__benny__list__.append("'+__benny__inlist__[0:__benny__inlist__.find('=')]+'")')
+					exec(__root__object__+'.'+__benny__object__+'.__benny__list__.append("'+__benny__inlist__[0:__benny__inlist__.find('=')].strip()+'")')
 					exec(__root__object__+'.'+__benny__object__+'.'+__benny__inlist__)
 			else:
 				if __benny__type__value__:
-					exec(__root__object__+'.__benny__list__.append("'+__benny__object__+'")')
+					exec(__root__object__+'.__benny__list__.append("'+__benny__object__.strip()+'")')
 					exec(__root__object__+'.'+__benny__object__+'='+__benny__value__)
 			if __root__object__=='':
 				__root__object__ = __benny__object__
