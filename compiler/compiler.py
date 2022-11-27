531be7889266ee9cf3d8ca87c60454ed25bb010d
Home Garden Control <109976409+homegardencontrol@users.noreply.github.com>
2022-11-27 10:35:48 +0700

    Create compiler.py

---
+++ compiler/compiler.py
@@ -0,0 +1,85 @@
+import sys,os
+print(os.getcwd())
+file_name = sys.argv[1]
+f = open(file_name, 'r')
+lines = f.readlines()
+out_text = ''
+__benny__class__ = []
+for line in lines:
+    #print('>>>',end=' ')
+    __benny__syntax__ = line
+    if __benny__syntax__.find('.')<0 and len(__benny__syntax__.split('['))==2 and __benny__syntax__.find('=')>__benny__syntax__.find('[') and __benny__syntax__.find('=')<__benny__syntax__.find(']'):
+        __benny__object__ = __benny__syntax__
+        __benny__list__ = __benny__object__[__benny__object__.find('[')+1:__benny__object__.find(']')].split(',')
+        __benny__object__=__benny__object__[0:__benny__object__.find('[')]
+        if not __benny__object__ in __benny__class__:
+            exec('class '+__benny__object__+':\n __benny__list__=[]\n pass')
+            __benny__class__.append(__benny__object__)
+        for __benny__inlist__ in __benny__list__:
+            #exec(__benny__object__+'.__benny__list__.append("'+__benny__inlist__[0:__benny__inlist__.find('=')].strip()+'")')
+            out_text = out_text + __benny__object__+'.__benny__list__.append("'+__benny__inlist__[0:__benny__inlist__.find('=')].strip()+'")\n'
+            #exec(__benny__object__+'.'+__benny__inlist__)
+            out_text = out_text + __benny__object__+'.'+__benny__inlist__+'\n'
+        continue
+    if __benny__syntax__.find('.')<__benny__syntax__.find('=') and __benny__syntax__.find('.')>=0:
+        if len(__benny__syntax__.split('['))>2 or len(__benny__syntax__.split(']'))>2 or __benny__syntax__.find('{')>=0:
+            print('Benny error more than [1 block] in sentense and dict not allow')
+            exit()
+        if __benny__syntax__[0:__benny__syntax__.find('=')].find('(')>=0:
+            #exec(__benny__syntax__)
+            out_text = out_text + __benny__syntax__
+            continue
+        __benny__object__list__ = __benny__syntax__.split('.')
+        __root__object__ = ''
+        __root__dot__object__ = ''
+        for __benny__object__ in __benny__object__list__:
+            __benny__list__type__ = False
+            if __benny__object__.find('[')>0 and __benny__object__[__benny__object__.find('[')+1:__benny__object__.find(']')].find('=')>=0:
+                __benny__list__ = __benny__object__[__benny__object__.find('[')+1:__benny__object__.find(']')].split(',')
+                __benny__object__=__benny__object__[0:__benny__object__.find('[')]
+                __benny__list__type__ = True
+            __benny__type__value__ = False
+            if __benny__object__.find('=')>0:
+                __benny__value__ = __benny__object__[__benny__object__.find('=')+1:]
+                __benny__object__=__benny__object__[0:__benny__object__.find('=')]
+                __benny__type__value__ = True
+            if __root__object__ == '':
+                if not __benny__object__ in __benny__class__:
+                    #exec('class '+__benny__object__+':\n __benny__list__=[]\n pass')
+                    __benny__class__.append(__benny__object__)
+                    out_text = out_text +'class '+__benny__object__+':\n __benny__list__=[]\n pass\n'
+            else:
+                if not(__benny__type__value__):
+                    if not __root__object__+'_'+__benny__object__ in __benny__class__:
+                        #exec('class '+__root__object__+'_'+__benny__object__+':\n __benny__list__=[]\n pass')
+                        __benny__class__.append(__root__object__+'_'+__benny__object__)
+                        out_text = out_text+'class '+__root__object__+'_'+__benny__object__+':\n __benny__list__=[]\n pass\n'
+                        #exec(__root__dot__object__+'.'+__benny__object__+'='+__root__object__+'_'+__benny__object__)
+                        out_text = out_text + __root__dot__object__+'.'+__benny__object__+'='+__root__object__+'_'+__benny__object__+'\n'
+            if __benny__list__type__:
+                for __benny__inlist__ in __benny__list__:
+                    #exec(__root__object__+'.'+__benny__object__+'.__benny__list__.append("'+__benny__inlist__[0:__benny__inlist__.find('=')].strip()+'")')
+                    out_text = out_text + __root__object__+'.'+__benny__object__+'.__benny__list__.append("'+__benny__inlist__[0:__benny__inlist__.find('=')].strip()+'")\n'
+                    #exec(__root__object__+'.'+__benny__object__+'.'+__benny__inlist__)
+                    out_text = out_text + __root__object__+'.'+__benny__object__+'.'+__benny__inlist__+'\n'
+            else:
+                if __benny__type__value__:
+                    #exec(__root__object__+'.__benny__list__.append("'+__benny__object__.strip()+'")')
+                    out_text = out_text + __root__object__+'.__benny__list__.append("'+__benny__object__.strip()+'")\n'
+                    #exec(__root__object__+'.'+__benny__object__+'='+__benny__value__)
+                    out_text = out_text + __root__object__+'.'+__benny__object__+'='+__benny__value__+'\n'
+            if __root__object__=='':
+                __root__object__ = __benny__object__
+                __root__dot__object__ = __benny__object__
+            else:
+                __root__object__ = __root__object__+'_'+__benny__object__
+                __root__dot__object__ = __root__dot__object__+'.'+__benny__object__
+    else:
+        #exec(__benny__syntax__)
+        out_text = out_text + __benny__syntax__ +'\n'
+
+file_name = file_name[:file_name.find('.')]
+file_name = file_name+'.py'
+f = open(file_name, 'w')
+f.write(out_text)
+f.close()
