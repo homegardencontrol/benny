7fed0f3cf9f28410e092d27ffb008e216f69eb98
Home Garden Control <109976409+homegardencontrol@users.noreply.github.com>
2022-11-29 16:17:44 +0700

    Update compiler.py

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -23,17 +23,34 @@ for line in lines:
                 __line__indent__ = i
             else:
                 break
-    __benny__syntax__ = line[__indent__:].strip()+'\n'
+    if line.find('#')>=0 :
+        __benny__syntax__ = line[0:line.find('#')].strip()
+        if __benny__syntax__=='':
+            if line.find('#')>=__indent__:
+                out_text = out_text +line[__indent__:]
+            else:
+                out_text = line
+            continue
+    else:
+        __benny__syntax__ = line[__indent__:].strip()
     if __benny__syntax__.find('if ')>=0 or __benny__syntax__.find('for ')>=0 or __benny__syntax__.find('while ')>=0 or __benny__syntax__.find('match ')>=0 or __benny__syntax__.find('case ')>=0:
-        out_text = out_text +line[__indent__:__line__indent__]+ __benny__syntax__
+        out_text = out_text +line[__indent__:__line__indent__]+ __benny__syntax__+'\n'
         continue
+    # non . before [] and non . in []
     if __benny__syntax__.find('.')<0 and len(__benny__syntax__.split('['))==2 and __benny__syntax__.find('=')>__benny__syntax__.find('[') and __benny__syntax__.find('=')<__benny__syntax__.find(']'):
         __benny__object__ = __benny__syntax__
         __benny__list__ = __benny__object__[__benny__object__.find('[')+1:__benny__object__.find(']')].split(',')
         __benny__object__=__benny__object__[0:__benny__object__.find('[')]
-        if not __benny__object__ in __benny__class__:
-            #exec('class '+__benny__object__+':\n __benny__list__=[]\n pass')
-            out_text = out_text +line[__indent__:__line__indent__]+ 'class '+__benny__object__+':\n __benny__list__=[]\n pass\n'
+        try: # call exist class
+            exec('test = '+__benny__object__)
+            out_text = out_text +line[__indent__:__line__indent__]+ __benny__syntax__+'\n'
+            continue
+        except:
+            if not __benny__object__ in __benny__class__:
+                #exec('class '+__benny__object__+':\n __benny__list__=[]\n pass')
+                out_text = out_text+line[__indent__:__line__indent__]+'class '+__benny__object__+':\n'
+                out_text = out_text+line[__indent__:__line__indent__]+' __benny__list__=[]\n'
+                out_text = out_text+line[__indent__:__line__indent__]+' pass\n'
             __benny__class__.append(__benny__object__)
         for __benny__inlist__ in __benny__list__:
             #exec(__benny__object__+'.__benny__list__.append("'+__benny__inlist__[0:__benny__inlist__.find('=')].strip()+'")')
@@ -41,7 +58,8 @@ for line in lines:
             out_text = out_text +line[__indent__:__line__indent__]+ ' '+ __benny__object__+'.__benny__list__.append("'+__benny__inlist__[0:__benny__inlist__.find('=')].strip()+'")\n'
             #exec(__benny__object__+'.'+__benny__inlist__)
             out_text = out_text +line[__indent__:__line__indent__]+ __benny__object__+'.'+__benny__inlist__+'\n'
-            continue
+        continue
+    # have . before []
     if __benny__syntax__.find('.')<__benny__syntax__.find('=') and __benny__syntax__.find('.')>=0:
         if len(__benny__syntax__.split('['))>2 or len(__benny__syntax__.split(']'))>2 or __benny__syntax__.find('{')>=0:
             print('Benny error more than [1 block] in sentense and dict not allow')
@@ -50,11 +68,28 @@ for line in lines:
             #exec(__benny__syntax__)
             out_text = out_text +line[__indent__:__line__indent__]+ __benny__syntax__
             continue
-        __benny__object__list__ = __benny__syntax__.split('.')
+        #print(__benny__syntax__)
+        if __benny__syntax__.find('[')>=0:
+            __benny__object__list__ = __benny__syntax__[0:__benny__syntax__.find('[')].split('.')
+            __benny__object__list__[len(__benny__object__list__)-1] = __benny__object__list__[len(__benny__object__list__)-1]+__benny__syntax__[__benny__syntax__.find('['):]
+        else:
+            if __benny__syntax__.find('=')>=0:
+                __benny__object__list__ = __benny__syntax__[0:__benny__syntax__.find('=')].split('.')
+                __benny__object__list__[len(__benny__object__list__)-1] = __benny__object__list__[len(__benny__object__list__)-1]+__benny__syntax__[__benny__syntax__.find('='):]
+            else:
+                __benny__object__list__ = __benny__syntax__.split('.')
+        #print(__benny__object__list__)
+        try: # call exist class
+            exec('test = '+__benny__object__list__[0])
+            out_text = out_text +line[__indent__:__line__indent__]+ __benny__syntax__+'\n'
+            continue
+        except:
+            pass
         __root__object__ = ''
         __root__dot__object__ = ''
         for __benny__object__ in __benny__object__list__:
             __benny__list__type__ = False
+            __benny__type__value__ = False
             if __benny__object__.find('[')>0 and __benny__object__[__benny__object__.find('[')+1:__benny__object__.find(']')].find('=')>=0:
                 __benny__list__ = __benny__object__[__benny__object__.find('[')+1:__benny__object__.find(']')].split(',')
                 __benny__object__=__benny__object__[0:__benny__object__.find('[')]
@@ -68,13 +103,17 @@ for line in lines:
                 if not __benny__object__ in __benny__class__:
                     #exec('class '+__benny__object__+':\n __benny__list__=[]\n pass')
                     __benny__class__.append(__benny__object__)
-                    out_text = out_text +line[__indent__:__line__indent__]+'class '+__benny__object__+':\n __benny__list__=[]\n pass\n'
+                    out_text = out_text+line[__indent__:__line__indent__]+'class '+__benny__object__+':\n'
+                    out_text = out_text+line[__indent__:__line__indent__]+' __benny__list__=[]\n'
+                    out_text = out_text+line[__indent__:__line__indent__]+' pass\n'
             else:
                 if not(__benny__type__value__):
                     if not __root__object__+'_'+__benny__object__ in __benny__class__:
                         #exec('class '+__root__object__+'_'+__benny__object__+':\n __benny__list__=[]\n pass')
                         __benny__class__.append(__root__object__+'_'+__benny__object__)
-                        out_text = out_text+line[__indent__:__line__indent__]+'class '+__root__object__+'_'+__benny__object__+':\n __benny__list__=[]\n pass\n'
+                        out_text = out_text+line[__indent__:__line__indent__]+'class '+__root__object__+'_'+__benny__object__+':\n'
+                        out_text = out_text+line[__indent__:__line__indent__]+' __benny__list__=[]\n'
+                        out_text = out_text+line[__indent__:__line__indent__]+' pass\n'
                         #exec(__root__dot__object__+'.'+__benny__object__+'='+__root__object__+'_'+__benny__object__)
                         out_text = out_text +line[__indent__:__line__indent__]+ __root__dot__object__+'.'+__benny__object__+'='+__root__object__+'_'+__benny__object__+'\n'
             if __benny__list__type__:
